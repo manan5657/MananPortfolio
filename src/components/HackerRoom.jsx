a878ece79493f2d27458e681fcd7668ddbbf7285
Manan Bhasin <mananbhasin5657@gmail.com>
2025-01-11 21:09:37 +0530

    RockPoison first commit

---
+++ src/components/HackerRoom.jsx
@@ -0,0 +1,113 @@
+import { useGLTF, useTexture } from "@react-three/drei";
+import { useEffect, useState, useRef } from "react";
+import * as THREE from "three";
+
+const HackerRoom = (props) => {
+  const { nodes, materials } = useGLTF("/models/hacker-room.glb");
+  const [videoTexture, setVideoTexture] = useState(null);
+  const videoRef = useRef();
+
+  useEffect(() => {
+    const video = document.createElement("video");
+    video.src = "textures/desk/screenprototype2.mp4"; // Ensure the video file path is correct
+    video.loop = true;
+    video.muted = true;
+    video.playsInline = true; // Helps with autoplay in some browsers
+    video.crossOrigin = "anonymous"; // Avoid CORS issues
+    video
+      .play()
+      .then(() => {
+        const texture = new THREE.VideoTexture(video);
+        texture.encoding = THREE.sRGBEncoding;
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        texture.wrapS = THREE.ClampToEdgeWrapping;
+        texture.wrapT = THREE.ClampToEdgeWrapping;
+
+        setVideoTexture(texture);
+      })
+      .catch((error) => {
+        console.error("Error playing video", error);
+      });
+
+    videoRef.current = video;
+
+    // Cleanup function to stop video when component unmounts
+    return () => {
+      video.pause();
+      video.src = "";
+      videoRef.current = null;
+    };
+  }, []);
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        geometry={nodes.screen_screens_0.geometry}
+        material={materials.screens}
+      >
+        {videoTexture && <meshBasicMaterial map={videoTexture} />}
+      </mesh>
+      <mesh
+        geometry={nodes.screen_glass_glass_0.geometry}
+        material={materials.glass}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_1.geometry}
+        material={materials.table_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_2.geometry}
+        material={materials.computer_mat}
+      >
+        <meshMatcapMaterial map={useTexture("textures/desk/monitor.png")} />
+      </mesh>
+      <mesh
+        geometry={nodes.table_table_mat_0_3.geometry}
+        material={materials.server_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_4.geometry}
+        material={materials.vhsPlayer_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_5.geometry}
+        material={materials.stand_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_6.geometry}
+        material={materials.mat_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_7.geometry}
+        material={materials.arm_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_8.geometry}
+        material={materials.tv_mat}
+      >
+        <meshMatcapMaterial map={useTexture("textures/desk/monitor.png")} />
+      </mesh>
+      <mesh
+        geometry={nodes.table_table_mat_0_9.geometry}
+        material={materials.cables_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_10.geometry}
+        material={materials.props_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_11.geometry}
+        material={materials.ground_mat}
+      />
+      <mesh
+        geometry={nodes.table_table_mat_0_12.geometry}
+        material={materials.key_mat}
+      />
+    </group>
+  );
+};
+
+useGLTF.preload("/models/hacker-room.glb");
+
+export default HackerRoom;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,123 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import HackerRoom from "../components/HackerRoom.jsx";
+import { calculateSizes } from "../constants/index.js";
+import { useMediaQuery } from "react-responsive";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Rings from "../components/Rings.jsx";
+import Cube from "../components/Cube.jsx";
+import HeroCamera from "../components/HeroCamera.jsx";
+import Button from "../components/Button.jsx";
+// import {Leva, useControls} from "leva";
+
+const Hero = () => {
+  //! Leva Controls For Real Time Customization of Model ->
+  // const x = useControls('HackerRoom',{
+  //     positionX:{
+  //         value:2.5,
+  //         min:-10,
+  //         max:10
+  //     },
+  //     positionY:{
+  //         value:2.5,
+  //         min:-10,
+  //         max:10
+  //     },
+  //     positionZ:{
+  //         value:2.5,
+  //         min:-10,
+  //         max:10
+  //     },
+  //     rotationX:{
+  //         value:0,
+  //         min:-10,
+  //         max:10
+  //     },
+  //     rotationY:{
+  //         value:0,
+  //         min:-10,
+  //         max:10
+  //     },
+  //     rotationZ:{
+  //         value:0,
+  //         min:-10,
+  //         max:10
+  //     },
+  //     scale:{
+  //         value:1,
+  //         min:0.1,
+  //         max:10
+  //     },
+  // })
+  //!
+
+  // Use media queries to determine screen size
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I am Manan Bhasin <span className="waving-hand">👋🏻</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+
+      {/* Canvas */}
+      <div className="w-full h-full absolute inset-0">
+        {/*<Leva hidden />*/}
+
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            {/* To hide controller */}
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+
+            <HeroCamera isMobile={isMobile}>
+              <HackerRoom
+                scale={sizes.deskScale}
+                position={sizes.deskPosition}
+                rotation={[0.1, -Math.PI, 0]}
+              />
+
+              {/*For Checking and tweaking any model values by sliding in leva */}
+              {/*<HackerRoom scale={[x.scale,x.scale,x.scale]} position={[x.positionX,x.positionY,x.positionZ]}*/}
+              {/*            rotation={[x.rotationX,x.rotationY,x.rotationZ]}/>*/}
+            </HeroCamera>
+
+            {/* Side Elements Group */}
+            <Target position={sizes.targetPosition} />
+            <ReactLogo position={sizes.reactLogoPosition} />
+            <Rings position={sizes.ringPosition} />
+            <Cube position={sizes.cubePosition} />
+            {/* */}
+
+            {/*Lights*/}
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#about" className="w-fit">
+          <Button
+            name="Let's work together"
+            isBeam
+            containerClass="sm:w-fit w-full sm:min-w-96"
+          />
+        </a>
+      </div>
+    </section>
+  );
+};
+export default Hero;
